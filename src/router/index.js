a2e5ce482938cc9e16c7d21bdab6087b515f50f7
CallMeNephilim <mahdiafkar0@gmail.com>
2026-09-15 22:41:52 +1000

    changing index  n Bheader

--- src/router/index.js
+++ src/router/index.js
@@ -1,46 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue'
-import LoginView from '../views/LoginView.vue'
-import AccessDeniedView from '../views/AccessDeniedView.vue'
-import { isAuthenticated } from '../auth'
+import FirebaseSigninView from '../views/FirebaseSigninView.vue'
+import FirebaseRegisterView from '../views/FirebaseRegisterView.vue'
+import AddBookView from '../views/AddBookView.vue'
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'About',
-    component: AboutView,
-    meta: { requiresAuth: true }
-  },
-  {
-    path: '/login',
-    name: 'Login',
-    component: LoginView
-  },
-  {
-    path: '/access-denied',
-    name: 'AccessDenied',
-    component: AccessDeniedView
-  }
+  { path: '/', name: 'Home', component: HomeView },
+  { path: '/about', name: 'About', component: AboutView },
+  { path: '/FireLogin', name: 'FireLogin', component: FirebaseSigninView },
+  { path: '/FireRegister', name: 'FireRegister', component: FirebaseRegisterView },
+  { path: '/addbook', name: 'AddBook', component: AddBookView }
 ]
 
 const router = createRouter({
   history: createWebHistory(),
   routes
-})
-
-// Navigation Guard
-router.beforeEach((to, from, next) => {
-  if (to.meta.requiresAuth && !isAuthenticated.value) {
-    next({ name: 'Login' })
-  } else {
-    next()
-  }
 })
 
 export default router
